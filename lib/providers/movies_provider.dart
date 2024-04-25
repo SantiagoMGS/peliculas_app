2eca267a6bb6a9174c9c1edc99c2f6f2651cb0aa
Santiago Martinez <smartinez9626@gmail.com>
2024-04-25 14:14:34 -0500

    obtain Movies response from model

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:peliculas_app/models/models.dart';
+
 class MoviesProvider extends ChangeNotifier {
   MoviesProvider() {
     print('MoviesProvider constructor');
@@ -21,7 +23,7 @@ class MoviesProvider extends ChangeNotifier {
 
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
-    final Map<String, dynamic> decodedData = json.decode(response.body);
-    print(decodedData['dates']);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    print(nowPlayingResponse.results[0].title);
   }
 }
