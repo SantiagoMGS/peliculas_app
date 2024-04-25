ebfeeb92dd8a496b0c6439412091581a7ea1bee8
Santiago Martinez <smartinez9626@gmail.com>
2024-04-25 13:49:04 -0500

    get response from api

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,10 +1,27 @@
-class MoviesProvider {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MoviesProvider extends ChangeNotifier {
   MoviesProvider() {
     print('MoviesProvider constructor');
     this.getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('On Display Movies');
+    String _apiKey = '4b95e8a54096c1d6576b09e0399de53a';
+    String _baseUrl = 'api.themoviedb.org';
+    String _language = 'es-ES';
+
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData['dates']);
   }
 }
