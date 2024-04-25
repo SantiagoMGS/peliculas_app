90bf6730815473d52021238c4d2a70c1487cc635
Santiago Martinez <smartinez9626@gmail.com>
2024-04-25 13:18:53 -0500

    movies_provider created

---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,10 @@
+class MoviesProvider {
+  MoviesProvider() {
+    print('MoviesProvider constructor');
+    this.getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    print('On Display Movies');
+  }
+}
