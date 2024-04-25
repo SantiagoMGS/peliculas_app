6be71bca858fb897aee3aa134018dd59a800e109
Santiago Martinez <smartinez9626@gmail.com>
2024-04-25 14:56:21 -0500

    format code on movies_provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -5,16 +5,17 @@ import 'dart:convert';
 import 'package:peliculas_app/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
+  final String _apiKey = '4b95e8a54096c1d6576b09e0399de53a';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
+
+  List<Movie> onDisplayMovies = [];
+
   MoviesProvider() {
-    print('MoviesProvider constructor');
-    this.getOnDisplayMovies();
+    getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    String _apiKey = '4b95e8a54096c1d6576b09e0399de53a';
-    String _baseUrl = 'api.themoviedb.org';
-    String _language = 'es-ES';
-
     var url = Uri.https(_baseUrl, '3/movie/now_playing', {
       'api_key': _apiKey,
       'language': _language,
@@ -24,6 +25,8 @@ class MoviesProvider extends ChangeNotifier {
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
     final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
-    print(nowPlayingResponse.results[0].title);
+
+    onDisplayMovies = nowPlayingResponse.results;
+    notifyListeners();
   }
 }
